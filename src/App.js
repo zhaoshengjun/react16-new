283dee6f01a12496bca2e3b21d0c8d7d076cbd24
Shengjun Zhao <Shengjun.Zhao@dsiti.qld.gov.au>
2017-09-28 11:11:16 +1000

    catch error

--- src/App.js
+++ src/App.js
@@ -7,20 +7,33 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: { name: "Chris" }
+      user: { name: "Chris" },
+      hasError: false
     };
   }
   updateUser = () => {
     this.setState(state => ({ ...state, user: null }));
   };
 
+  componentDidCatch(err, info) {
+    console.log("error", err);
+    console.log("info", info);
+    this.setState(state => ({ ...state, hasError: true }));
+    // good place to send the error to error reporting
+    // sendToErrorReporting(error, info)
+  }
+
   render() {
-    return (
-      <div>
-        <Profile user={this.state.user} />
-        <button onClick={this.updateUser}>Update</button>
-      </div>
-    );
+    if (this.state.hasError) {
+      return <div>Sorry, something went wrong!</div>;
+    } else {
+      return (
+        <div>
+          <Profile user={this.state.user} />
+          <button onClick={this.updateUser}>Update</button>
+        </div>
+      );
+    }
   }
 }
 
