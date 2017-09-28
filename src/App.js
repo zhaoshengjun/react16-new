38f3b2406f0c6618207cdc57ee97d23c795467c9
Shengjun Zhao <Shengjun.Zhao@dsiti.qld.gov.au>
2017-09-28 11:18:56 +1000

    wrap error catch as a higher order component

--- src/App.js
+++ src/App.js
@@ -3,37 +3,47 @@ import "./App.css";
 
 const Profile = props => <div>Name: {props.user.name}</div>;
 
-class App extends Component {
+class MyErrorBoundry extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: { name: "Chris" },
       hasError: false
     };
   }
-  updateUser = () => {
-    this.setState(state => ({ ...state, user: null }));
-  };
 
   componentDidCatch(err, info) {
-    console.log("error", err);
-    console.log("info", info);
     this.setState(state => ({ ...state, hasError: true }));
-    // good place to send the error to error reporting
-    // sendToErrorReporting(error, info)
+    // sendToErrorReporting(err, info);
   }
 
   render() {
     if (this.state.hasError) {
       return <div>Sorry, something went wrong!</div>;
     } else {
-      return (
-        <div>
-          <Profile user={this.state.user} />
-          <button onClick={this.updateUser}>Update</button>
-        </div>
-      );
+      return this.props.children;
     }
+  }
+}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: { name: "Chris" }
+    };
+  }
+  updateUser = () => {
+    this.setState(state => ({ ...state, user: null }));
+  };
+
+  render() {
+    return (
+      <div>
+        <MyErrorBoundry>
+          <Profile user={this.state.user} />
+        </MyErrorBoundry>
+        <button onClick={this.updateUser}>Update</button>
+      </div>
+    );
   }
 }
 
